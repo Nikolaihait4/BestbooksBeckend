8aad4cf1aa6b9756e5068f3e3ad2653a0c00790b
Nikolaihait4 <jet1nikolai@gmail.com>
2024-03-10 10:39:46 +0100

    dataBase
    dataBase - connect

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
+// import express from "express";
+// import morgan from "morgan";
+// import cors from "cors";
+const mongoose = require("mongoose");
 
-import contactsRouter from "./routes/contactsRouter.js";
+// import contactsRouter from "./routes/contactsRouter.js";
 
-const app = express();
+// const app = express();
 
-app.use(morgan("tiny"));
-app.use(cors());
-app.use(express.json());
+const DB_Host =
+  "mongodb+srv://Haitjet:w4NGVdpKuYh39Usl@cluster0.hi9qo5h.mongodb.net/books_reader?retryWrites=true&w=majority";
+mongoose
+  .connect(DB_Host)
+  .then(() => console.log("Database connect sucess"))
+  .catch((error) => console.log(error.message));
 
-app.use("/api/contacts", contactsRouter);
+// app.use(morgan("tiny"));
+// app.use(cors());
+// app.use(express.json());
 
-app.use((_, res) => {
-  res.status(404).json({ message: "Route not found" });
-});
+// app.use("/api/contacts", contactsRouter);
 
-app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
-});
+// app.use((_, res) => {
+//   res.status(404).json({ message: "Route not found" });
+// });
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+// app.use((err, req, res, next) => {
+//   const { status = 500, message = "Server error" } = err;
+//   res.status(status).json({ message });
+// });
+
+// app.listen(3000, () => {
+//   console.log("Server is running. Use our API on port: 3000");
+// });
